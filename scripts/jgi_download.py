85aed498d1b6b33ad165194de4b1acc205be25bd
Jason Stajich <jasonstajich.phd@gmail.com>
2019-11-15 11:17:57 -0800

    migrate to Mycocosm download

--- scripts/jgi_download.py
+++ scripts/jgi_download.py
@@ -5,149 +5,90 @@ import os, sys, logging, csv, re
 import xml.etree.ElementTree as ET
 DEBUG=1
 
+long2shortasm = { "Mitochondrial assembly": 'mito',
+                  "Assembled scaffolds (unmasked)": 'nuclear' }
 
-# SOME FUNCTIONS TO HELP
-# GET ALL THE FILES
-def get_files(folder,dtype,species):
-    for file in folder.findall('file'):
-        name = file.get('label')
-        url  = file.get('url')
-        if name in local_names:
-            name = local_names[name]
+def get_assemblies (fileroot):
+    for dir in fileroot.findall("folder"):
+#        print("->",dir.tag,dir.attrib)
+        dirname = dir.get('name')
+        if ( dirname == "Mitochondrial assembly" or
+             dirname == "Assembled scaffolds (unmasked)"):
+            asmtype = long2shortasm[dirname]
 
-        if name not in species:
-            species[name] = {dtype: [url,name]}
-        elif dtype not in species[name]:
-            species[name][dtype] = [url,name]
-        else:
-            print("warning - updating %s %s with url (%s) when it was previously %s"
-                  % (name,dtype,url,species[name][dtype][0]))
-            species[name][dtype] = [url, name]
-    return species
+            for file in dir:
+                # print(file.tag, file.attrib)
+                url = file.get('url')
+                filename = file.get('filename')
+                label = file.get('label')
+                name = ascii(label)
+#                if filename.startswith('Aciri1_iso'):
+#                    name=name + " iso"
+                thisasmtype = asmtype
+                if( "MitoAssembly" in filename or
+                    'mito.scaffolds' in filename or
+                    'MitoScaffolds' in filename):
+                    thisasmtype = 'mito'
+                elif "PlasmidAssembly" in filename:
+                    thisasmtype = 'plasmid'
 
-#GET ALL THE TARFILES
-def get_tarfiles(folder,species):
-    for file in folder.findall('file'):
-        name = file.get('label')
-        url  = file.get('url')
-        filename = file.get('filename')
-        dtype = ""
-        if name in local_names:
-            name = local_names[name]
+                if name not in species:
+                    species[name] = {thisasmtype: [url,filename]}
+#                    print("storing %s for %s file=%s"%(name,thisasmtype,filename))
+                elif thisasmtype not in species[name]:
+                    species[name][thisasmtype] = [url,filename]
+#                    print("storing %s for %s file=%s"%(name,thisasmtype,filename))
+                else:
+                    print("skipping this data as already have a type %s stored for %s - trying to save %s" %(thisasmtype,name,filename))
+                    print(species[name])
 
-        m = re.search(r'(\S+).tar.gz$',filename)
-        if ( m ):
-            dtype = 'tar'
-            # print("%s match for %s outpref = %s"%(dtype,filename,name))
-        else:
-            continue
+# we probably should defined a priority here based on age of file
+# primary alleles over secondary, etc
+# for now relying on the XML file order and how it is parsed as to who
+# wins
+def get_annotations(fileroot):
+    for dir in fileroot.findall("folder"):
+        print("->",dir.tag,dir.attrib)
+        dirname = dir.get('name')
 
-        if name not in species:
-            species[name] = {dtype: [url,name]}
-        elif dtype not in species[name]:
-            species[name][dtype] = [url,name]
-        else:
-            print("warning - updating %s %s with url (%s) when it was previously %s"
-                  % (name,dtype,url,species[name][dtype][0]))
-            species[name][dtype] = [url, name]
-    return species
+        for file in dir:
+            if file.tag == "file":
+                url = file.get('url')
+                filename = file.get('filename')
+                if ("secondary_alleles" in filename or
+                    'Secondary_Alleles' in filename):
+                    continue
 
-#GET THE ANNOTATION FILES
-def get_annotation_files(folder,species):
-    for file in folder.findall('file'):
-        name = file.get('label')
-        url  = file.get('url')
-        filename = file.get('filename')
-        dtype = ""
-        if name in local_names:
-            name = local_names[name]
-        m = re.search(r'([^_]+)_(\S+).(gff|tsv|domtblout)$',filename)
-        if ( m ):
-            dtype = m.group(3)
-            name = name + "_" + m.group(2)
-            # print("%s match for %s outpref = %s"%(dtype,filename,name))
-        elif re.search(r'_(prodigal|genemark)',filename):
-            continue
-        elif re.search(r'_contigs\.fna',filename):
-            dtype = "DNA"
-        elif re.search(r'_genes\.fna',filename):
-            dtype = "CDS"
-        elif re.search(r'_proteins\.faa',filename):
-            dtype = "pep"
-        else:
-            #print("Cannot match filename %s to a category"%(filename))
-            continue
-#        print("'%s' '%s'"%(filename,name))
+                label = file.get('label')
+                name = ascii(label)
+#                if filename.startswith('Aciri1_iso'):
+#                    name=name + " iso"
 
-        if name not in species:
-            species[name] = {dtype: [url,name]}
-        elif dtype not in species[name]:
-            species[name][dtype] = [url,name]
-        else:
-            print("warning - updating %s %s with url (%s) when it was previously %s"
-                  % (name,dtype,url,species[name][dtype][0]))
-            species[name][dtype] = [url, name]
-    return species
+                dtype = ''
 
-def traverse(xmlroot,species):
+                if filename.endswith("gff3.gz") or filename.endswith("gff.gz"):
+                    dtype = 'gff'
+                elif dirname == "CDS":
+                    dtype = 'CDS'
 
-    for topfolder in xmlroot.findall('folder'):
-        foldername = topfolder.get('name')
-        if (  foldername == 'Sequence' or foldername == 'Assembly' or
-              foldername == 'Alignment'):
-            for filesfolder in topfolder.findall("folder"):
-                print("names are ",filesfolder.get("name"))
-                if filesfolder.get('name') == "QC and Genome Assembly":
-                    if foldername == 'Assembly':
-                        species = get_files(filesfolder,foldername+"-dna",species)
-                    for subfolder in filesfolder.findall("folder"):
-                        species = get_files(subfolder,foldername,species)
-                elif filesfolder.get('name') == "IMG Data":
-                    if foldername == 'Assembly':
-                        species = get_annotation_files(filesfolder,species)
-        elif foldername == "Annotation":
-            for filesfolder in topfolder.findall("folder"):
-                if filesfolder.get('name') == "IMG Data":
-                    species = get_annotation_files(filesfolder,species)
-        elif foldername == "Other":
-            for filesfolder in topfolder.findall("folder"):
-                if filesfolder.get('name') == "IMG Data":
-                    species = get_tarfiles(filesfolder,species)
-    return species
-
-#INIT SOME VARIABLES
-hosturl='https://genome.jgi.doe.gov'
-
-mapext = {'Sequence'   : [ 'fastq', 'corr.fastq.gz'],
-          'Assembly'  : [ 'info', 'coverage.txt' ],
-          'Assembly-dna'   : [ 'DNA' , 'fasta'],
-          'domtblout' : ['annotation', 'domtblout'],
-#          'blasttab' : ['annotation', 'blasttab'],
-          'gff' : ['annotation', "gff"],
-          'tsv' : ['annotation', "tsv"],
-          'DNA'   : [ 'DNA' , 'contig.fasta'],
-          'pep'   : [ 'pep' , 'aa.fasta'],
-          'CDS'   : [ 'CDS' , 'cds.fasta'],
-          'Alignment'  : [ 'SAM', 'sam.gz' ],
-          'tar'   : [ 'Other', 'tar.gz' ],
-
-      }
-
-outdir="source/JGI"
-
-if not os.path.exists(os.path.dirname(outdir)):
-    os.mkdir(os.path.dirname(outdir))
-
-# make the top level folder for storing the data
-if not os.path.exists(outdir):
-    os.mkdir(outdir)
-# make the folders for storing the data - process mapext dictionary
-for t in mapext.values():
-    if not os.path.exists(os.path.join(outdir,t[0])):
-        os.mkdir(os.path.join(outdir,t[0]))
+                if dtype:
+                    if name not in species:
+                        species[name] = {dtype: [url,filename]}
+#                        print("storing %s for %s file=%s"%(name,dtype,filename))
+                    elif dtype not in species[name]:
+                        species[name][dtype] = [url,filename]
+#                        print("storing %s for %s file=%s"%(name,dtype,filename))
+                    else:
+                        # replace gff3 with gff when available
+                        if (filename.endswith(".gff3.gz") and
+                            species[name][dtype][1].endswith(".gff.gz")):
+                            species[name][dtype] = [url,filename]
+                        else:
+                            print("skipping this data as already have a type %s stored for %s - trying to save %s" %(dtype,name,filename))
+                            print(species[name])
 
 
-# READ IN SOME SKIP DIRECTIVES
+
 skipme='lib/skip_data_jgi.csv'
 
 skip_data = {}
@@ -159,17 +100,29 @@ if os.path.exists(skipme):
             if len(row):
                 skip_data[row[0]] = 1
 
-local_names = {}
+outdir="source/JGI"
+if not os.path.exists(os.path.dirname(outdir)):
+    os.mkdir(os.path.dirname(outdir))
+if not os.path.exists(outdir):
+    os.mkdir(outdir)
+for t in ['CDS','GFF','DNA','MT','pep']:
+    if not os.path.exists(os.path.join(outdir,t)):
+        os.mkdir(os.path.join(outdir,t))
+
+
+hosturl='https://genome.jgi.doe.gov'
 
 # HARCODED THE EXPECTED NAME OF THE INPUT NAMES
 localnamesfile = 'lib/zygo_names.csv'
+#local_names = {}
+#if os.path.exists(localnamesfile):
+#    with open(localnamesfile,'r') as newnm:
+#        nmread = csv.reader(newnm,delimiter="\t")
+#        header = next(nmread)
+#        for row in nmread:
+#            local_names[row[0]] = row[1]
 
-if os.path.exists(localnamesfile):
-    with open(localnamesfile,'r') as newnm:
-        nmread = csv.reader(newnm,delimiter="\t")
-        header = next(nmread)
-        for row in nmread:
-            local_names[row[0]] = row[1]
+species = {}
 
 names = ['mucoromycota', 'zoopagomycota']
 if len(sys.argv) > 1: # if a command line argument is provided
@@ -178,38 +131,56 @@ if len(sys.argv) > 1: # if a command line argument is provided
 for base in names:
     xmlfile  = "lib/%s.xml" % (base)
 
-    if not os.path.exists(xmlfile):
-        print("expecting %s - did you run scripts/init_jgi_download.sh?"%(xmlfile))
-        exit()
-
-    species = {}
     tree = ET.parse(xmlfile)
     root = tree.getroot()
-    for topfolder in root.findall('folder'):
-        foldername = topfolder.get('name')
-        #print("top",foldername)
-        for subfolder in topfolder.findall('folder'):
-            #print("\tsub1",subfolder.get('name'))
-            if subfolder.get('name') == "Files":
-                species = traverse(subfolder,species)
 
+    # main code
+    for topfolder in root.findall('folder'):
+        if topfolder.get('name') == 'Unknown':
+            for filesfolder in topfolder.findall("folder"):
+                if filesfolder.get('name') == "Files":
+                    for files in filesfolder.findall("folder"):
+                        print("\t",files.tag,files.attrib)
+                        if files.get('name') == "Assembly":
+                            get_assemblies(files)
+                        elif files.get('name') == "Annotation":
+                            for annot in files.findall('folder'):
+                                if annot.get('name') == 'Filtered Models ("best")':
+                                    get_annotations(annot)
+
+mapext = {'nuclear': [ 'DNA', 'nt.fasta.gz'],
+          'mito'   : [ 'MT' , 'mt.fasta.gz'],
+          'gff'    : [ 'GFF', 'gff3.gz' ],
+          'CDS'    : [ 'CDS', 'cds.fasta.gz'] }
+
+for base in names:
+    xmlfile  = "lib/%s.xml" % (base)
     # main code
     with open("lib/%s.csv"%(base),"w") as jgiout:
         with open("lib/%s_jgi_download.sh"%(base),"w") as dwnload:
             jgicsv = csv.writer(jgiout,delimiter=",",lineterminator="\n")
-            jgicsv.writerow(['Label','read_URL','asm_URL'])
+            jgicsv.writerow(['Prefix','Species','Full Name','DNA_URL','MITO_URL',
+                             'GFF_URL','CDS_URL'])
             for sp in sorted(species.keys()):
-                row = ['',sp]
-                for t in mapext.keys():
+                s_sp = re.sub(r'\s*v\d+\.\d+\s*','',sp)
+                s_sp = re.sub(r'\s*\(Environmental single-cell\)\s*','',s_sp)
+                s_sp = re.sub(r'\s+',' ',s_sp)
+                s_sp = re.sub(r'\'','',s_sp)
+                print(s_sp)
+                m = re.split(r'\s+',s_sp,maxsplit=3)
+                print(m)
+                row = [''," ".join(m[0:2]),sp]
+                for t in ['nuclear','mito','gff','CDS']:
                     if t in species[sp]:
                         if len(row[0]) == 0:
-                            row[0] = species[sp][t][1]
+                            row[0] = species[sp][t][0].split('/')[2]
                         row.append(hosturl + species[sp][t][0])
                         outfile="%s.%s"%(os.path.join(outdir,mapext[t][0],
                                                       row[0]),mapext[t][1])
-                        if ( not os.path.exists(outfile) and
-                        not os.path.exists(outfile+".gz") ):
+                        if not os.path.exists(outfile):
                             dwnload.write("curl -o %s '%s' -b cookies\n"
                                           %(outfile,row[-1]))
                     else:
                         row.append("NO_%s_URL"%(t))
+                if row[0] not in skip_data:
+                    jgicsv.writerow(row)
